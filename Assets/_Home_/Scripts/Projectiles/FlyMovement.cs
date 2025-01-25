00506e64baa130c280f11e5ed4b1e90e12a0ea4f
Dani <danithegamedev@gmail.com>
2025-01-25 22:53:00 +0100

    prefabs added, player movement updated

---
+++ Assets/_Home_/Scripts/Projectiles/FlyMovement.cs
@@ -0,0 +1,47 @@
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+public class flyMovement : MonoBehaviour
+{
+    public float maxSpeed = 1.5f;
+    public float turnSpeed = 3f;
+    public float acceleration = 1f;
+    private float speed = 0;
+    private GameObject Target;
+    private GameObject[] players;
+
+    void Start() 
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        Target = players[0];
+    }
+
+    void Update()
+    {
+        selectTarget();
+        rotateTowards(Target.transform.position);
+        advance();
+    }
+    
+    void rotateTowards(Vector3 target) {
+        Quaternion _lookRotation =  Quaternion.LookRotation((target - transform.position).normalized);
+        transform.rotation =  Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * turnSpeed);
+    }
+
+    void advance()
+    {
+        speed += acceleration * Time.deltaTime;
+        if (speed > maxSpeed) {speed = maxSpeed;}
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+    
+    void selectTarget()
+    {
+        foreach (GameObject player in players)
+        {
+            float _distanceToPlayer = Vector3.Distance (player.transform.position, transform.position);
+            float _distanceToTarget = Vector3.Distance (Target.transform.position, transform.position);
+            if (_distanceToPlayer < _distanceToTarget) {Target = player;}
+        }
+    }
+}
