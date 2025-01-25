4cd133d1e533269b99eceb52855a6128d333ecff
Javier Riera Chirivella <mrvizious@gmail.com>
2025-01-25 11:20:42 +0100

    Removed unnecessary states

--- Assets/_Home_/Scripts/Boss/Boss State Machine/EndAbilityBossState.cs
+++
@@ -1,7 +0,0 @@
-using DesignPatterns;
-using UnityEngine;
-using UnityEngine.InputSystem;
-
-public class EndAbilityBossState : AbilityBossState
-{
-}
--- Assets/_Home_/Scripts/Boss/Boss State Machine/MoveBossState.cs
+++ Assets/_Home_/Scripts/Boss/Boss State Machine/MoveBossState.cs
@@ -5,15 +5,34 @@ using UnityEngine.InputSystem;
 
 public class MoveBossState : BossState
 {
+    public InputActionReference moveAction;
+    private Vector2 moveValue;
     public override async UniTask ProcessInputEvent(InputAction.CallbackContext inputContext)
     {
-        Ability ability = bossController.abilityActions[inputContext.action];
-        if (ability != null)
+        if (bossController.abilityActions.ContainsKey(inputContext.action))
         {
-            StartAbilityBossState newState = (StartAbilityBossState)stateMachine.PrepareState(typeof(StartAbilityBossState));
+            Ability ability = bossController.abilityActions[inputContext.action];
+            ChargeAbilityBossState newState = (ChargeAbilityBossState)stateMachine.PrepareState(typeof(ChargeAbilityBossState));
             newState.ability = ability;
             stateMachine.ChangeToState(newState);
             return;
         }
+
+        if (inputContext.action == moveAction.action)
+        {
+            moveValue = inputContext.ReadValue<Vector2>().normalized;
+            Debug.Log($"Move value is {moveValue}", this);
+        }
+
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        bossController.abilityStartTransform.position += Vector3.forward * moveValue.y * Time.deltaTime;
     }
 }
