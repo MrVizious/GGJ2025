456d20ae016eceab62c57e41124ac82202519353
Javier Riera Chirivella <mrvizious@gmail.com>
2025-01-25 10:34:31 +0100

    Variety pool

--- Assets/_Home_/Scripts/Pools/VarietyPool.cs
+++ Assets/_Home_/Scripts/Pools/VarietyPool.cs
@@ -1,23 +1,46 @@
+using System;
 using System.Collections.Generic;
 using DesignPatterns;
 using Sirenix.OdinInspector;
-using UnityEngine;
+using Sirenix.Serialization;
 
-public class VarietyPool : SerializedMonoBehaviour
+public class VarietyPool : DesignPatterns.Singleton<VarietyPool>
 {
+    protected override bool dontDestroyOnLoad => false;
     [ReadOnly]
-    [ShowInInspector]
-    Dictionary<string, Pool<Poolable>> pools = new Dictionary<string, Pool<Poolable>>();
+    [OdinSerialize]
+    private Dictionary<Type, Poolable> prefabs = new Dictionary<Type, Poolable>();
+    private Dictionary<Type, Pool<Poolable>> pools = new Dictionary<Type, Pool<Poolable>>();
 
     [Button]
-    public void AddPrefab(Poolable newPoolable)
+    public void AddPrefab(Poolable newPrefab)
     {
-        pools.Add(newPoolable.GetType().ToString(), new Pool<Poolable>(prefab: newPoolable));
+        Type newType = newPrefab.GetType();
+        if (prefabs.ContainsKey(newType)) return;
+        prefabs.Add(newType, newPrefab);
     }
 
-    public void AddPrefab<T>(T newPoolable) where T : Poolable
+
+    public T Get<T>(bool needsPrefab = true) where T : Poolable
     {
-
+        if (pools.ContainsKey(typeof(T)))
+        {
+            return (T)pools[typeof(T)].Get();
+        }
+        if (prefabs.ContainsKey(typeof(T)))
+        {
+            var prefab = prefabs[typeof(T)];
+            Pool<Poolable> newPool = new Pool<Poolable>(defaultCapacity: 350, maxSize: 1000, newObjectName: typeof(T).ToString(), prefab: prefab, parent: transform);
+            pools.Add(typeof(T), newPool);
+            return (T)pools[typeof(T)].Get();
+        }
+        if (!needsPrefab)
+        {
+            Pool<Poolable> newPool = new Pool<Poolable>(newObjectName: typeof(T).ToString(), parent: transform);
+            pools.Add(typeof(T), newPool);
+            return (T)pools[typeof(T)].Get();
+        }
+        throw new ArgumentException("Can't get an instance of the given type because no prefab is provided to the factory" +
+                                "Try adding one or setting \"needsPrefab\" to false so a simple instance can be created.");
     }
-
 }
