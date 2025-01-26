824f55ec118d0232fc78177fd3709eb4a22ad445
Javier Riera Chirivella <mrvizious@gmail.com>
2025-01-26 11:04:14 +0100

    Cojones selector

---
+++ Assets/_Home_/Scripts/Game Data/Cojones.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Cojones", menuName = "Decisiones importantísimas/Cojones")]
+public class Cojones : ScriptableObject
+{
+    public bool cojones = true;
+}
